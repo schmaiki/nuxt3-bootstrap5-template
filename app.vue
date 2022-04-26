fc3c190ad5056290bc3a057d6b7e4e66683e3edf
Maiki <73941088+schmaiki@users.noreply.github.com>
2022-04-26 09:09:31 +0200

    Update app.vue
    navbar umgesetzt

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="bg-theme1 p-3">
-      <h3>Minimal Boostrap5 Template</h3>
-      <br />
       <NavBar />
+      <br>
+      <h1>Minimal Boostrap5 Template</h3>
     </div>
   </div>
 </template>
